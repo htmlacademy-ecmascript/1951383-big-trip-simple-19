87b5cb3b167ac4ab8aa2fd857b1f0d4cc233f7ad
AlexandraLeksy <_leksy_@list.ru>
2022-12-14 23:55:02 +0300

    2.15. Шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import TripPresenter from './presenter/trip-presenter.js';
+import PointsModel from './model/points-model.js';
 // Контейнер для фильтров
 const headerElement = document.querySelector('.trip-controls');
 // Контейнер для отрисовки
 const tripEventsElement = document.querySelector('.trip-events');
+// Модель
+const pointsModel = new PointsModel();
 // Новый презентер с параметрами
 const tripPresenter = new TripPresenter({
   boardContainer: tripEventsElement,
   filterContainer: headerElement,
+  pointsModel
 });
 
 tripPresenter.init();
--- src/view/trip-point-view.js
+++ src/view/trip-point-view.js
@@ -1,44 +1,72 @@
 
 import {createElement} from '../render.js';
+import { destinations, offersByTypes } from '../mock/mock.js';
+import dayjs from 'dayjs';
 
-function createPointTemplate() {
-  return (
-    `<li class="trip-events__item">
-    <div class="event">
-      <time class="event__date" datetime="2019-03-18">MAR 18</time>
-      <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
-      </div>
-      <h3 class="event__title">Taxi Amsterdam</h3>
-      <div class="event__schedule">
-        <p class="event__time">
-          <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
-          &mdash;
-          <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+const DATE_FORMAT_DATE = 'DD MMM';
+const DATE_FORMAT_TIME = 'HH:mm';
+
+const createPointTemplate = (point) =>{
+  const {type, offers, destination, basePrice, dateFrom, dateTo} = point;
+  const pointTypeOffer = offersByTypes.find((offer) => offer.type === type);
+  const pointDestination = destinations.find((item) => destination === item.id);
+  const checkedOffers = pointTypeOffer.offers
+    .filter((offer) => offers.includes(offer.id));
+
+  const offersTemplate = () => {
+    if (!checkedOffers.length) {
+      return `<li class="event__offer">
+    <span class="event__offer-title">No additional offers</span>
+    </li>`;
+    } else {
+      const template = checkedOffers.map((offer) => `<li class="event__offer">
+      <span class="event__offer-title">${offer.title}</span>
+      &plus;&euro;&nbsp;
+      <span class="event__offer-price">${offer.price}</span>
+    </li>`).join('');
+      return template;
+    }
+  };
+
+
+  const parceDateStart = dayjs(dateFrom);
+  const parceDateEnd = dayjs(dateTo);
+
+  return `<li class="trip-events__item">
+      <div class="event">
+        <time class="event__date" datetime="${dateFrom}">${parceDateStart.format(DATE_FORMAT_DATE)}</time>
+        <div class="event__type">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
+        </div>
+        <h3 class="event__title">${type} ${pointDestination.name}</h3>
+        <div class="event__schedule">
+          <p class="event__time">
+            <time class="event__start-time" datetime="${dateFrom}">${parceDateStart.format(DATE_FORMAT_TIME)}</time>
+            &mdash;
+            <time class="event__end-time" datetime="${dateTo}">${parceDateEnd.format(DATE_FORMAT_TIME)}</time>
+          </p>
+        </div>
+        <p class="event__price">
+          &euro;&nbsp;<span class="event__price-value">${basePrice}</span>
         </p>
+        <h4 class="visually-hidden">Offers:</h4>
+        <ul class="event__selected-offers">
+          ${offersTemplate()}
+        </ul>
+        <button class="event__rollup-btn" type="button">
+          <span class="visually-hidden">Open event</span>
+        </button>
       </div>
-      <p class="event__price">
-        &euro;&nbsp;<span class="event__price-value">20</span>
-      </p>
-      <h4 class="visually-hidden">Offers:</h4>
-      <ul class="event__selected-offers">
-        <li class="event__offer">
-          <span class="event__offer-title">Order Uber</span>
-          &plus;&euro;&nbsp;
-          <span class="event__offer-price">20</span>
-        </li>
-      </ul>
-      <button class="event__rollup-btn" type="button">
-        <span class="visually-hidden">Open event</span>
-      </button>
-    </div>
-  </li>`
-  );
-}
+    </li>`;
+};
 
 export default class PointView {
+  constructor({point}) {
+    this.point = point;
+  }
+
   getTemplate() {
-    return createPointTemplate();
+    return createPointTemplate(this.point);
   }
 
   getElement() {
